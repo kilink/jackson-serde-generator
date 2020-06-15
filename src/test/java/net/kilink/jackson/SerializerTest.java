705fd92f2f084cd803cbe3ef2078105b5d5af6b3
Patrick Strawderman <patrick@kilink.net>
2020-06-15 16:56:55 -0700

    Add test that instantiates generated serializer

--- src/test/java/net/kilink/jackson/SerializerTest.java
+++ src/test/java/net/kilink/jackson/SerializerTest.java
@@ -2,15 +2,27 @@ package net.kilink.jackson;
 
 import static com.google.testing.compile.CompilationSubject.assertThat;
 import static com.google.testing.compile.Compiler.javac;
+import static net.kilink.jackson.TestUtils.loadClass;
+import static org.junit.Assert.assertEquals;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.testing.compile.Compilation;
 import com.google.testing.compile.JavaFileObjects;
 import org.junit.Test;
 
+import javax.tools.JavaFileObject;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class SerializerTest {
 
@@ -67,17 +79,50 @@ public class SerializerTest {
     }
 
     @Test
-    public void testSerializerGeneration() {
+    public void testSerializerGeneration() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
 
         String generatedSerializer = Util.generateSerializer(Foo.class, mapper.getSerializationConfig());
+        JavaFileObject sourceFile = JavaFileObjects.forSourceString(
+                "com.kilink.net.serializers.FooSerializer", generatedSerializer);
 
-        System.out.println(generatedSerializer);
-
-        Compilation result = javac()
-                .compile(JavaFileObjects.forSourceString(
-                        "com.kilink.net.serializers.FooSerializer", generatedSerializer));
-
+        Compilation result = javac().compile(sourceFile);
         assertThat(result).succeededWithoutWarnings();
+
+        JavaFileObject classFile = result.generatedFiles().get(0);
+        Class<JsonSerializer<Foo>> serializerClass = loadClass(classFile);
+
+        JsonSerializer<Foo> serializer = serializerClass.newInstance();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Foo.class, serializer);
+        mapper.registerModule(module);
+        mapper.disable(MapperFeature.AUTO_DETECT_CREATORS,
+                MapperFeature.AUTO_DETECT_FIELDS,
+                MapperFeature.AUTO_DETECT_GETTERS,
+                MapperFeature.AUTO_DETECT_SETTERS,
+                MapperFeature.AUTO_DETECT_IS_GETTERS,
+                MapperFeature.USE_ANNOTATIONS);
+
+        Foo foo = new Foo();
+        foo.setBaz(42);
+        foo.setBar("barValue");
+        foo.setEnabled(true);
+        foo.setThings(ImmutableList.of("foo", "bar" , "baz"));
+        foo.setOtherThings(ImmutableMap.of("key", "value"));
+
+        String serializedJson = mapper.writeValueAsString(foo);
+        JsonNode jsonNode = mapper.readTree(serializedJson);
+
+        assertEquals(foo.getBaz(), jsonNode.at("/baz").asInt());
+        assertEquals(foo.getBar(), jsonNode.at("/renamedBar").asText());
+        assertEquals(foo.isEnabled(), jsonNode.at("/enabled").asBoolean());
+
+        List<String> things = StreamSupport.stream(jsonNode.at("/things").spliterator(), false)
+                .map(JsonNode::asText)
+                .collect(Collectors.toList());
+        assertEquals(foo.getThings(), things);
+
+        Map<String, String> otherThings = mapper.convertValue(jsonNode.at("/otherThings"), new TypeReference<Map<String, String>>() {});
+        assertEquals(foo.getOtherThings(), otherThings);
     }
 }
---
+++ src/test/java/net/kilink/jackson/TestUtils.java
@@ -0,0 +1,36 @@
+package net.kilink.jackson;
+
+import com.google.common.io.ByteStreams;
+
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public final class TestUtils {
+    private static class FileObjectClassLoader extends ClassLoader {
+        private <T> Class<T> defineClassFromFileObject(JavaFileObject classFile) {
+            final byte[] bytes;
+            try (InputStream is = classFile.openInputStream()) {
+                bytes = ByteStreams.toByteArray(is);
+            } catch (IOException ioe) {
+                throw new UncheckedIOException(ioe);
+            }
+
+            @SuppressWarnings("unchecked")
+            Class<T> klazz = (Class<T>) defineClass(null, bytes, 0, bytes.length);
+            return klazz;
+        }
+    }
+
+    private static final FileObjectClassLoader classLoader = new FileObjectClassLoader();
+
+    public static <T> Class<T> loadClass(JavaFileObject classFile) {
+        checkNotNull(classFile);
+        checkArgument(classFile.getKind() == JavaFileObject.Kind.CLASS);
+        return classLoader.defineClassFromFileObject(classFile);
+    }
+}
